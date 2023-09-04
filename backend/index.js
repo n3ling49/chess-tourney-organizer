56989cb619fdccdaf8b241b9fa1b130dd132de3f
n3ling49 <noahdreiling@gmail.com>
2023-09-04 18:08:40 +0200

    make server use register route

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const port = 5000;
 
 const swiss = require("./swiss");
 
+const registerRoute = require("./routes/register-route");
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect("mongodb://localhost:27019/chesstourney")
   .then(() => {console.log("Connected to MongoDB")})
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.use("/api/register", registerRoute);
 
 app.get("/standings", (req, res) => {
   const standings = swiss.sortPlayers();
