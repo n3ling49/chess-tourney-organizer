d549b1f65138a526f1691444f4a3c809b3bcf74e
n3ling49 <noahdreiling@gmail.com>
2023-09-04 17:37:22 +0200

    make server connect to db on startup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const port = 5000;
 
 const swiss = require("./swiss");
 
 app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27019/chesstourney")
+  .then(() => {console.log("Connected to MongoDB")})
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 app.get("/standings", (req, res) => {
   const standings = swiss.sortPlayers();
@@ -24,5 +30,5 @@ app.get("/players", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
